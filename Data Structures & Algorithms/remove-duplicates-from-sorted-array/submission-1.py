4b5e0ae394c0a4ff5e5e4dba1964a586ee081ec1
bahimehdi <153679390+bahimehdi@users.noreply.github.com>
2026-08-23 19:20:32 +0100

    Add: remove-duplicates-from-sorted-array - submission-1

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-1.py
@@ -0,0 +1,14 @@
+# Ascending order list
+# task: remove dups, return nums with no dups
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        l, r = 0, len(nums)
+        k = 0
+        while l < r:
+            used = nums[l]
+            while l < r and nums[l] == used:
+                l += 1
+            nums[k] = used
+            k += 1
+        return k
