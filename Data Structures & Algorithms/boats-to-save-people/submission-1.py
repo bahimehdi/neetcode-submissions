0bdebb0d8c4d2c248b562935b2436aa190136a28
bahimehdi <153679390+bahimehdi@users.noreply.github.com>
2026-08-23 21:51:57 +0100

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.py
@@ -0,0 +1,23 @@
+# list of people, people[i] = weight of person i
+# a boat can 1-2 people, a boat can carry up to limit in weight
+# return min(boats) where every person is carried
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        l, r = 0, len(people) - 1
+        boats = 0
+        people = sorted(people)
+        while l < r:
+            while (people[l] + people[r] > limit) and l <= r:
+                boats += 1
+                r -= 1
+            while (people[l] + people[r] == limit) and l < r:
+                boats += 1
+                l += 1
+                r -= 1
+            if people[l] + people[r] < limit:
+                boats += 1
+                l += 1
+                r -= 1
+        if l == r:
+            boats += 1
+        return boats
