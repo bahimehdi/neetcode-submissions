d08f9d9cd84d352ef8bb08023f884eed9ebbd8ad
bahimehdi <153679390+bahimehdi@users.noreply.github.com>
2026-08-24 22:58:24 +0100

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,21 @@
+# rules: return true if
+# every open bracket `(,[,{` is closed by the same type of close bracket `},],)`
+# open brackets are closed in the correct order
+# every close bracket has a corresponding open bracket of the same type
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        brackets = {'{': '}', '[': ']', '(': ')'}
+        for i in s:
+            if not stack and i in brackets.values():
+                return False
+            if i in brackets:
+                stack.append(i)
+            elif i == brackets[stack[-1]]:
+                stack.pop()
+            else:
+                return False
+        if stack:
+            return False
+        return True
