59e9addd256dcfa1cc1971e18e81edad37de16a5
bahimehdi <153679390+bahimehdi@users.noreply.github.com>
2026-08-31 22:53:40 +0100

    Add: find-minimum-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-2.py
@@ -0,0 +1,16 @@
+# - Input
+# nums = sorted array in ascending order, rotated by k
+# - Output
+# find the k and return min(nums) in log(n)
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        n = len(nums)
+        L, R = 0, n - 1
+        while L < R:
+            M = (L + R) // 2
+            if nums[M] > nums[R]:
+                L = M + 1
+            elif nums[M] < nums[R]:
+                R = M
+        return nums[L]
