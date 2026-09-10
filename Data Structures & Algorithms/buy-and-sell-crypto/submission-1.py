5a5c8baaa2682b5939bb606b03ff3b080a48f6b6
bahimehdi <153679390+bahimehdi@users.noreply.github.com>
2026-09-10 01:36:39 +0100

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,16 @@
+# Sliding window
+# dynamic
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = j = 0
+        minimum = float('inf')
+        for i in range(len(prices)):
+            if minimum >= prices[i]:
+                j = i
+                minimum = prices[i]
+            while j < len(prices):
+                profit = max(profit, prices[j] - minimum)
+                j += 1
+            j = i
+        return profit
