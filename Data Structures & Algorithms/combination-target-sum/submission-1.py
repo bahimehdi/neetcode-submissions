b1b4ea09ed51ee229e7096edc3bedc8670129dce
bahimehdi <153679390+bahimehdi@users.noreply.github.com>
2026-09-16 23:50:14 +0100

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.py
@@ -0,0 +1,25 @@
+# The base case -> sum(path) == target
+# Choices -> recurse with nums[i], recurse with nums[i+1]
+# Constraints -> sum(path) > target
+# Backtracking step -> pop the last number added
+
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def backtrack(i, path):
+            if sum(path) == target:
+                res.append(path[:])
+                return
+            
+            if i == len(nums) or sum(path) > target:
+                return
+
+            path.append(nums[i])
+            backtrack(i, path)
+            path.pop()
+            backtrack(i + 1, path)
+            
+        
+        backtrack(0, [])
+        return res
