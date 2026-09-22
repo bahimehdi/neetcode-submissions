8bf143a2ea03f345b98160b5ef9b7403b2c359ff
bahimehdi <153679390+bahimehdi@users.noreply.github.com>
2026-09-22 21:31:31 +0100

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.py
@@ -0,0 +1,37 @@
+# tasks
+# identical tasks are separated by at least n (task[i] == next task)
+# return min CPU cycles to complete
+
+import heapq
+from collections import deque
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        least = 0
+        maxHeap = []
+
+        counter = {}
+        for t in tasks:
+            if t not in counter:
+                counter[t] = -1
+            else:
+                counter[t] -= 1
+
+        maxHeap = [(count, task) for (task, count) in counter.items()]
+        heapq.heapify(maxHeap)
+        queue = deque()
+
+        while maxHeap or queue:
+            if queue and queue[0][0] == least:
+                heapq.heappush(maxHeap, (queue[0][1], queue[0][2]))
+                queue.popleft()
+
+            if maxHeap:
+                if maxHeap[0][0] != -1:
+                    queue.append((least + n + 1, maxHeap[0][0] + 1, maxHeap[0][1]))
+                heapq.heappop(maxHeap)
+            least += 1
+            if queue and not maxHeap:
+                least = queue[0][0]
+
+        return least
